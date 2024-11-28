7bc2a3cad710aea20520f61bfcbe86416c76d65b
Albert <albert.obychenie@moe.ru>
2024-11-28 23:41:29 +0500

    Add FillFromFile and update FillingLists and ArrayCreatorMenu

--- src/filllists/FillingLists.java
+++ src/filllists/FillingLists.java
@@ -8,32 +8,25 @@ import models.Human;
 import java.util.ArrayList;
 import java.util.List;
 
-// Не стал доделывать до конца. Расставить индексы, как в indexModel
-// в примере, который нашли в интернете
 
 public class FillingLists {
     private final static int maxYear = 120;
     private final static int minYear = 0;
-    // Можно заполнить так и парсить и делать что-то похожее на это:
-    // String input = "MAN";
-    // Gender gender = Gender.valueOf(input);
     private final static List<List<String>> humanSurnameList = List.of(
-            List.of("Покровская","WOMAN"),
+            List.of("Покровская", "WOMAN"),
             List.of("Филиппова", "WOMAN"),
-            List.of("Леонов","MAN"),
+            List.of("Леонов", "MAN"),
             List.of("Кочетков", "MAN"),
             List.of("Тарасова", "WOMAN"),
             List.of("Козлов", "MAN"),
             List.of("Соколова", "WOMAN"),
-            List.of("Лаптева","WOMAN"),
-            List.of("Усов","MAN"),
-            List.of("Никонов","MAN")
+            List.of("Лаптева", "WOMAN"),
+            List.of("Усов", "MAN"),
+            List.of("Никонов", "MAN")
     );
 
-    // как добавить объем
-    private final static int maxVolume = 100;
-    private final static int minVolume = 5;
-    private final static List<List<String>> barrelList = List.of(
+
+    private final static List<List<String>> barrelListFill = List.of(
             List.of("Water", "Plastic", "100"),
             List.of("Soda", "Glass", "50"),
             List.of("Coke", "Cardboard", "30"),
@@ -41,7 +34,6 @@ public class FillingLists {
             List.of("Coconut milk", "Can", "40")
     );
 
-    // как добавить булевую шерсть
     private final static List<List<String>> animalListFill = List.of(
             List.of("Fish", "Black", "false"),
             List.of("Bird", "Orange", "true"),
@@ -49,25 +41,23 @@ public class FillingLists {
             List.of("Reptiles", "Yellow", "false"),
             List.of("Amphibians", "Blue", "false")
     );
-    // Можно попробовать использовать Boolean.parseBoolean()
-//    private final static List<Boolean> animalList_isHair = List.of(true, false);
 
-    public List fillAnimalList(int count) {
-        List animalList;
+    public List<Animal> fillAnimalList(int count) {
+        List<Animal> animalList = new ArrayList<>();
         if (count > 0) {
-            animalList = new ArrayList<Animal>(count);
-            String kind, yearColor;
-            boolean isHair = true;
 
             for (int i = 0; i < count; i++) {
-                kind = String.valueOf(generateRandomInRangeIncludeEnd(0, animalList_Kind.size()));
-                yearColor = String.valueOf(generateRandomInRangeNotIncludeEnd(0, animalList_EyeColor.size()));
-                indexRootVegetable = generateRandomInRangeNotIncludeEnd(0, animalList_isHair.size());
+                List<String> ls = animalListFill.get(generateRandomInRangeNotIncludeEnd(0, animalListFill.size()));
 
-                animalList.add(new Animal().Builder()
-                        .type(TYPE_ROOT_VEGETABLE_LIST.get(indexRootVegetable))
-                        .weight(weight)
-                        .color(COLOR_LIST.get(indexColor))
+                String k = ls.get(0);
+                String c = ls.get(1);
+                boolean h = Boolean.valueOf(ls.get(2));
+
+                animalList.add(new Animal
+                        .Builder()
+                        .kind(k)
+                        .eyeColor(c)
+                        .isHair(h)
                         .build());
             }
         } else {
@@ -76,24 +66,21 @@ public class FillingLists {
         return animalList;
     }
 
-    public List fillHumanList(int count) {
-        List humanList;
+    public List<Human> fillHumanList(int count) {
+        List<Human> humanList = new ArrayList<>();
         if (count > 0) {
-            humanList = new ArrayList<Human>(count);
-            int age, indexSurname;
-            Gender gender1 = Gender.WOMAN;
-            Gender gender2 = Gender.WOMAN;
-            // как инициализировать
+
 
             for (int i = 0; i < count; i++) {
-                age = generateRandomInRangeIncludeEnd(minYear, maxYear);
-                // и добавить его вот здесь
-                gender = genderDiff ? gender1 : gender2;
-                indexSurname = generateRandomInRangeNotIncludeEnd(0, humanSurnameList.size());
-                humanSurnameList.add(new Human.Builder().
+                int age = generateRandomInRangeIncludeEnd(minYear, maxYear);
+
+                List<String> ls = humanSurnameList.get(generateRandomInRangeNotIncludeEnd(0, humanSurnameList.size()));
+                String n = ls.get(0);
+                Gender g = Gender.valueOf(ls.get(1));
+                humanList.add(new Human.Builder().
                         age(age).
-                        gender(gender).
-                        surname(humanSurnameList.get(indexSurname)).
+                        gender(g).
+                        surname(n).
                         build());
             }
         } else {
@@ -102,24 +89,20 @@ public class FillingLists {
         return humanList;
     }
 
-    public List fillBarrelList(int count) {
-        List barrelList;
+    public List<Barrel> fillBarrelList(int count) {
+        List<Barrel> barrelList = new ArrayList<>();
         if (count > 0) {
-            barrelList = new ArrayList<Barrel>(count);
-            String storedMaterial, barrelMaterial;
-            int volume;
-            int hash;
 
             for (int i = 0; i < count; i++) {
-                ;
-                storedMaterial = String.valueOf(generateRandomInRangeNotIncludeEnd(0, barrelList.size()));
-                barrelMaterial = String.valueOf(generateRandomInRangeNotIncludeEnd(0, barrel_List_Material.size()));
-                volume = generateRandomInRangeIncludeEnd(minVolume, maxVolume);
-//                hash = hashCode()...
+
+                List<String> bl = barrelListFill.get(generateRandomInRangeNotIncludeEnd(0, barrelList.size()));
+                String sm = bl.get(0);
+                String bm = bl.get(1);
+                int v = Integer.parseInt(bl.get(2));
                 barrelList.add(new Barrel.Builder()
-                        .storedMaterial(storedMaterial)
-                        .barrelMaterial(barrelMaterial)
-                        .volume(volume)
+                        .storedMaterial(sm)
+                        .barrelMaterial(bm)
+                        .volume(v)
                         .build());
             }
         } else {
@@ -140,4 +123,4 @@ public class FillingLists {
 }
 
 
-}
+
